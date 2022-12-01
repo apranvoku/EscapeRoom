62292c08fd63b0a86dd45293411944573cfb5454
Ardit Pranvoku <pranvoku.ardit@gmail.com>
2022-12-01 15:56:32 -0500

    SlidePuzzle Almost Done

---
+++ Assets/SlideManager.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideManager : MonoBehaviour
+{
+    public GameObject First;
+    public GameObject Second;
+    public GameObject Third;
+
+    public GameObject Fourth;
+    public GameObject Fifth;
+    public GameObject Sixth;
+
+    public GameObject Seventh;
+    public GameObject Eighth;
+
+    public GameObject[,] slidePuzzle;
+    public GameObject[,] solution;
+
+    public bool shuffled = false;
+    public bool solved = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slidePuzzle = new GameObject[,]
+        {
+            {First, Second, Third},
+            {Fourth, Fifth, Sixth },
+            {Seventh, Eighth, null }
+        };
+
+        solution = new GameObject[,]
+        {
+            {First, Second, Third},
+            {Fourth, Fifth, Sixth },
+            {Seventh, Eighth, null }
+        };
+
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<Tile>().AssignPosition();
+        }
+        for (int x = 0; x < 20; x++)
+        {
+            Shuffle();
+        }
+        shuffled = true;
+    }
+
+    public void isSolved()
+    {
+        for (int i = 0; i < slidePuzzle.GetLength(0); i++)
+        {
+            for (int j = 0; j < slidePuzzle.GetLength(1); j++)
+            {
+                int childIndex = i * 3 + j; //Ex: 3rd row 1st column is child number 7. Remember 0 <= i <= 2 & 0<= j <= 2.
+                if(childIndex == 8) //There is no child index 8, SInce there are 8 tiles indexed from 0 - 7 So we're done.
+                {
+                    Debug.Log("Good job you dirty boy.");
+                    solved = true;
+                    return;
+                }
+                else if (slidePuzzle[i, j] == transform.GetChild(childIndex).gameObject)
+                {
+                    //Debug.Log("Child: " + childIndex + " is correctly at row column: " + i + ", " + j);
+                    continue;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+
+    }
+
+    public void Shuffle() //Attempt to shuffle random tiles.
+    {
+        int random = UnityEngine.Random.Range(0, 8);
+        transform.GetChild(random).GetComponent<Tile>().SlideTile();
+    }
+
+    public Tuple<int, int> GetIndex(GameObject[,] array, GameObject searchItem)
+    {
+        for(int i = 0; i < array.GetLength(0); i++)
+        { 
+            for(int j = 0; j < array.GetLength(1); j++)
+            {
+                if(array[i,j] == searchItem)
+                {
+                    return Tuple.Create(i, j);
+                }
+            }
+        }
+        return null;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Tuple<int, int> Position;
+    SlideManager SM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SM = transform.parent.GetComponent<SlideManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AssignPosition()
+    {
+        Position = SM.GetIndex(SM.slidePuzzle, transform.gameObject);
+        //Debug.Log(transform.gameObject.name + Position.Item1 + Position.Item2);
+    }
+
+    public void OnMouseDown()
+    {
+        SlideTile();
+    }
+
+    public void SlideTile()
+    {
+        int row = Position.Item1; //0 to 2
+        int column = Position.Item2; // 0 to 2
+
+        if (row + 1 < SM.slidePuzzle.GetLength(0) && SM.slidePuzzle[row+1, column] is null)
+        {
+            SlideDown(row, column);
+        }
+        if (column + 1 < SM.slidePuzzle.GetLength(1) && SM.slidePuzzle[row, column + 1] is null)
+        {
+            SlideRight(row, column);
+        }
+        if (row - 1 >= 0 && SM.slidePuzzle[row - 1, column] is null)
+        {
+            SlideUp(row, column);
+        }
+        if (column - 1 >= 0 && SM.slidePuzzle[row, column - 1] is null)
+        {
+            SlideLeft(row, column);
+        }
+        if(SM.shuffled)
+        {
+            //Debug.Log("Checking...");
+            SM.isSolved();
+        }
+    }
+    public void SlideDown(int row, int column)
+    {
+        SM.slidePuzzle[row, column] = null;
+        SM.slidePuzzle[row + 1, column] = transform.gameObject;
+        Position = Tuple.Create(row + 1, column);
+        transform.position += new Vector3(0, -2, 0);
+    }
+    public void SlideRight(int row, int column)
+    {
+        SM.slidePuzzle[row, column] = null;
+        SM.slidePuzzle[row, column + 1] = transform.gameObject;
+        Position = Tuple.Create(row, column + 1);
+        transform.position += new Vector3(2, 0, 0);
+    }
+    public void SlideUp(int row, int column)
+    {
+        SM.slidePuzzle[row, column] = null;
+        SM.slidePuzzle[row - 1, column] = transform.gameObject;
+        Position = Tuple.Create(row - 1, column);
+        transform.position += new Vector3(0, 2, 0);
+    }
+
+    public void SlideLeft(int row, int column)
+    {
+        SM.slidePuzzle[row, column] = null;
+        SM.slidePuzzle[row, column - 1] = transform.gameObject;
+        Position = Tuple.Create(row, column - 1);
+        transform.position += new Vector3(-2, 0, 0);
+    }
+}
